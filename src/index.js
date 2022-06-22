884b5dba2a894577837b0b9d8b99aed795c25b7b
iamprakharevich <iamprakharevich@yandex.ru>
2022-06-22 11:03:00 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){n*=-1}
+    let str = n.toString(), str_rev='';
+    for (let i = str.length-1; i >= 0; i--) {
+        str_rev += str[i];
+    }
+    return +str_rev;
 }
